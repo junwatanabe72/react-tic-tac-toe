e944181049bd5c7e99bcfdbce4899156acd4f0ca
jun_mac <junwatanabe72@gmail.com>
2020-04-26 18:21:42 +0900

    Updates

---
+++ precache-manifest.bb804d97245aafcca486a89f01b02c82.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0eaf6aa3985be252714415e8f45bcc63",
+    "url": "/react-tic-tac-toe/index.html"
+  },
+  {
+    "revision": "4eabcd5eb85d6e2b9fdf",
+    "url": "/react-tic-tac-toe/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "6f37db4283f6e7fcc414",
+    "url": "/react-tic-tac-toe/static/js/2.6e6eeb3c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-tic-tac-toe/static/js/2.6e6eeb3c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4eabcd5eb85d6e2b9fdf",
+    "url": "/react-tic-tac-toe/static/js/main.74edec8b.chunk.js"
+  },
+  {
+    "revision": "57208936ddd33bb86d2e",
+    "url": "/react-tic-tac-toe/static/js/runtime-main.8a07e9c0.js"
+  }
+]);
