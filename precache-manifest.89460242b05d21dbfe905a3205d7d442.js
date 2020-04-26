51d534b137357e38bea1a40381ab9a996719861e
jun_mac <junwatanabe72@gmail.com>
2020-04-26 17:33:45 +0900

    Updates

---
+++ precache-manifest.89460242b05d21dbfe905a3205d7d442.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "afd4c3c5b01811f8c9d7c3167543a172",
+    "url": "/index.html"
+  },
+  {
+    "revision": "75ee6ce97eccddb3ec3b",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "7023f2a8c9b36dbd5719",
+    "url": "/static/js/2.52a58242.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.52a58242.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "75ee6ce97eccddb3ec3b",
+    "url": "/static/js/main.60fe8d58.chunk.js"
+  },
+  {
+    "revision": "4e5816df902866f28d32",
+    "url": "/static/js/runtime-main.bfd56a94.js"
+  }
+]);
